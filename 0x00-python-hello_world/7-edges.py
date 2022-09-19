9fa2079202f9dbec7d2e24f81fac6e1db61060e4
Joel Inyang <joelinyang2003@gmail.com>
2022-09-19 14:17:25 -0700

    d

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -2,7 +2,7 @@
 word = "Holberton"
 first_3 = word[0:3]
 last_2 = word[7:9]
-mid_word = word[2
+mid_word = word[2:8]
 print(f"First 3 letters: {first_3}")
 print(f"Last 2 letters: {last_2}")
 print(f"Middle word: {mid_word}")
