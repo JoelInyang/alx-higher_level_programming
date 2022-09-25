b9ff93963367ca807a1ed8bcec73a28991671999
Joel Inyang <joelinyang2003@gmail.com>
2022-09-25 01:55:04 -0700

    d

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    li = my_list.reversed
+    for i in li:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    str = 0
+    for i in my_string
+    if i != "c or C":
+        str += i
+        return(str)
