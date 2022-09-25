dd45a8ff88c24aebb75f3bb89a75d2b662f0692a
Joel Inyang <joelinyang2003@gmail.com>
2022-09-25 03:14:09 -0700

    s

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 and idx > len(my_list):
         return(my_list)
-    else:
-        return(my_lisr(idx)
+    if type(my_list) is list:
+        return(my_lisr[idx])
