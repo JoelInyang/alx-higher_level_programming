1aed2b8b67a373b4c465cd7e96bfc024fa493eca
Joel Inyang <joelinyang2003@gmail.com>
2022-09-25 03:30:53 -0700

    s

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     st = 0
-    for i in my_string
-    if i != "c or C":
-        st += i
-        return(st)
+    for i in my_string:
+        if i != "c or C":
+            st += i
+            return (st)
