0841a90c7cd94970111e2b551ddc28cc808db6be
Joel Inyang <joelinyang2003@gmail.com>
2022-09-28 02:19:25 -0700

    d

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_str = len(sentence)
+    first_ch = sentence[0]
+    if len_str == 0:
+        first_ch = None
+    else:
+        first_ch = sentence[0]
+    return (len_str, first_ch)
