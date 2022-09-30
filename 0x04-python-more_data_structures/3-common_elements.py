c6d8278013a8a5871dc28cf0b59b4242bcc90488
Joel Inyang <joelinyang2003@gmail.com>
2022-09-30 11:01:37 -0700

    more data structure

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def common_elements(set_1, set_2):
     com_elm = set 1 & set 2
     return (com_elm)
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for key in sorted(a_dictionary.keys()):
         print("{:s}:{}".format(keys, a_dictionary[key]))
