d8825e3bba0c73fb79f3911acd1cc7e7e21d0a00
Joel Inyang <joelinyang2003@gmail.com>
2022-09-30 21:06:46 -0700

    s

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        for row in matrix:
-            for x in row:
-                return (x ** 2)
+     for row in matrix:
+        for x in row:
+            return (x ** 2)
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if element != search
-    else:
+    if element != search:
+
+    else replace:
         for element in my_list:
             return (element)
