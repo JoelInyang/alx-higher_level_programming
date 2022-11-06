a814500c8d5022c2921fba495ac7a754f2fac217
JoelInyang <joelinyang2003@gmail.com>
2022-11-06 05:46:56 -0800

    g

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,16 +10,13 @@ class Square:
     def __init__(self, size=0):
         """
         The __init__ function is the constructor function initializing size
+        Raises:
+            TypeError
+            ValueError
         """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.size = size
-
-    def area(self):
-        """
-        It returns the rea of the square which is an instance method
-        """
-        return self.__size ** 2
+            self.__size = size
