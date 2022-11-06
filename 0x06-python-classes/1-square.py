39c793e4701c424c070f82e6a95295699c6fc22d
JoelInyang <joelinyang2003@gmail.com>
2022-11-06 04:49:14 -0800

    f

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+"""Class that defines a square using a privste attribute"""
 class Square:
     '''Defines a square'''
     def __init__(self, size):
+        """Constructor function"""
         self.size = size
