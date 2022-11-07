b4ba447e8468c756f7ddff3404704a0dabaebc89
JoelInyang <joelinyang2003@gmail.com>
2022-11-07 07:44:45 -0800

    f

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -26,10 +26,10 @@ class Square:
     """
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     def area(self):
     """
@@ -41,10 +41,10 @@ class Square:
         """
         It prints # in form of a square therby returning a square
         """
-        if size == 0:
+        if self.__size == 0:
             print(" ")
         else:
             for i in range(self.__size):
                 for j in range(self.__size):
-                    print("#", end=" ")
+                    print("#", end="")
                 print()
