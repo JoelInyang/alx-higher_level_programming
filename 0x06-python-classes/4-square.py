796cd9af6ddd655ca2d99fdf4c18e0726e4e4239
JoelInyang <joelinyang2003@gmail.com>
2022-11-07 07:24:01 -0800

    g

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,15 +1,30 @@
 #!/usr/bin/python3
+"""
+It defines a class square that has attribute size, a getter and setter is implemented as well.
+"""
 
 class Square:
+    """
+    The class along with the size also defines the area.
+    """
     def __init__(self, size=0):
+        """
+        The __init__ is the constructor function that initializes size
+        """
         self.size = size
 
     @property
     def size(self):
+        """
+        It returns the size of the square
+        """
         return self._size
 
     @size.setter
     def size(self, value):
+        """
+        it raises exceptions and also returns the size of the square
+        """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -18,4 +33,7 @@ class Square:
             self._size = size
 
     def area(self):
+        """
+        It returns the area of the square by raising size to the power of 2
+        """
         return self._size ** 2
