b01725c9ab7b09d9d02956fdf1b5b173d7ee651f
JoelInyang <joelinyang2003@gmail.com>
2022-11-07 07:38:45 -0800

    f

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-
+"""
+It defines a class square that has attribute size, a getter and setter is implemented as well.
+"""
 class Square:
+    """
+       The class along with the size also defines the area.
+    """
     def __init__(self, size=0):
+    """
+    The __init__ is the constructor function that initializes size
+    """
         self.size = size
 
     @property
     def size(self):
+    """
+    It returns the size of the square
+    """
         return self._size
 
     @size.setter
     def size(self, value):
+    """
+    It raises exceptions and also returns the size of the square
+    """
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -18,9 +32,15 @@ class Square:
             self._size = size
 
     def area(self):
+    """
+    It returns the area of the square by raising size to the power of 2
+    """
         return self._size ** 2
 
     def my_print(self):
+        """
+        It prints # in form of a square therby returning a square
+        """
         if size == 0:
             print(" ")
         else:
