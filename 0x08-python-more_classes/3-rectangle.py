63e5b8bbc72ce2f9f733c00150ee2c6a8af929a0
JoelInyang <joelinyang2003@gmail.com>
2022-11-07 20:37:32 -0800

    d

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     Class of Rectangle with private attributes
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
