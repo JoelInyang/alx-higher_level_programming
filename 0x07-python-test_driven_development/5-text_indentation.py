ee4c747d844869986ce97fb00131ca6d4723c730
JoelInyang <joelinyang2003@gmail.com>
2022-11-08 19:58:06 -0800

    f

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -2,6 +2,8 @@
 """
 5-text_indentation.py will print blankline after any of these '.', ':', '?'
 """
+
+
 def text_indentation(text):
     """
     prints new lines after some specific character
@@ -9,7 +11,8 @@ def text_indentation(text):
     if type(text) is not string:
         raise TypeError('text must be a string')
 
-    string = text.replace('.', '.\n\n').replace('?', '?\n\n').replace(':', ':\n\n')
+    string = text.replace('.', '.\n\n').replace
+    ('?', '?\n\n').replace(':', ':\n\n')
     for i in range(len(text)):
         string = string.replace('\n ', '\n')
     print(string, end="")
