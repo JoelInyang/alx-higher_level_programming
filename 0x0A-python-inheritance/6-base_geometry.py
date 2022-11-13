42e7f7a648dde54b873f2ebe26019026983d2b08
JoelInyang <joelinyang2003@gmail.com>
2022-11-12 17:51:51 -0800

    f

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -5,7 +5,12 @@ A class BG that raises exception
 
 
 class BaseGeometry:
-    pass
-
+    """
+    It a BG class
+    """
     def area(self):
+        """
+        A method for calculating thw function,
+        it returns the function
+        """
         raise Exception('area() is not implemented')
