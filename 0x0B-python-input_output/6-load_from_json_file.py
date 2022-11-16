287bab09b36a2af00887a6b9bc856eed3c7b0a39
JoelInyang <joelinyang2003@gmail.com>
2022-11-16 01:20:33 -0800

    JSON FILES

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+A filethat creates an Object from  JSON file
+@author: Joel Inyang
+"""
+
+
+import json
+
+def load_from_json_file(filename):
+    """
+    a function that creates an Object from a “JSON file”
+    """
+
+    with open(filename, 'w', encoding='utf-8') as f:
+        return (json.load(f.read()))
