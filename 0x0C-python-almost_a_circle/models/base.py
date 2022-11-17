1b8671e8f63e0166fcf2138bf824c454c5d19412
JoelInyang <joelinyang2003@gmail.com>
2022-11-17 02:37:55 -0800

    d

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -3,12 +3,19 @@
 The class base is a super class
 """
 
+
 class Base:
+    """
+    A class which is the super class
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """
+        The constructor function
+        """
         if type(id) is not None:
             self.id = id
         else:
-            __class__.__nb_objects += 1
-            self.id = __class__.nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
